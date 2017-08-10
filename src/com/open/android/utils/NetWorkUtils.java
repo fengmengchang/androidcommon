9562bcd434dde2cd3cb5a786104d025b67050069
Xenon Chau <cto@ido-game.com>
2017-08-10 14:33:11 +0800

    网络类型

--- src/com/open/android/utils/NetWorkUtils.java
+++ src/com/open/android/utils/NetWorkUtils.java
@@ -14,9 +14,10 @@ package com.open.android.utils;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
 
 /**
- *****************************************************************************************************************************************************************************
+ ***************************************************************************************************************************************************************************** 
  * 
  * @author :fengguangjing
  * @createTime:2017-8-2上午9:52:56
@@ -24,16 +25,16 @@ import android.net.NetworkInfo;
  * @modifyTime:
  * @modifyAuthor:
  * @description:
- *****************************************************************************************************************************************************************************
+ ***************************************************************************************************************************************************************************** 
  */
 public class NetWorkUtils {
 	/**
-     * 检查当前网络是否可用
-     * 
-     * @param context
-     * @return
-     */
-    
+	 * 检查当前网络是否可用
+	 * 
+	 * @param context
+	 * @return
+	 */
+
 	public static boolean isNetworkAvailable(Context context) {
 		// 获取手机所有连接管理对象（包括对wi-fi,net等连接的管理）
 		ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -54,5 +55,41 @@ public class NetWorkUtils {
 			}
 		}
 		return false;
+	}
+
+	public static String getAPNType(Context context) {
+		// 结果返回值
+		String netType = "nono_connect";
+		// 获取手机所有连接管理对象
+		ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		// 获取NetworkInfo对象
+		NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+		// NetworkInfo对象为空 则代表没有网络
+		if (networkInfo == null) {
+			return netType;
+		}
+		// 否则 NetworkInfo对象不为空 则获取该networkInfo的类型
+		int nType = networkInfo.getType();
+		if (nType == ConnectivityManager.TYPE_WIFI) {
+			// WIFI
+			netType = "wifi";
+		} else if (nType == ConnectivityManager.TYPE_MOBILE) {
+			int nSubType = networkInfo.getSubtype();
+			TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+			// 4G
+			if (nSubType == TelephonyManager.NETWORK_TYPE_LTE && !telephonyManager.isNetworkRoaming()) {
+				netType = "4G";
+			} else if (nSubType == TelephonyManager.NETWORK_TYPE_UMTS || nSubType == TelephonyManager.NETWORK_TYPE_HSDPA || nSubType == TelephonyManager.NETWORK_TYPE_EVDO_0
+					&& !telephonyManager.isNetworkRoaming()) {
+				netType = "3G";
+				// 2G 移动和联通的2G为GPRS或EGDE，电信的2G为CDMA
+			} else if (nSubType == TelephonyManager.NETWORK_TYPE_GPRS || nSubType == TelephonyManager.NETWORK_TYPE_EDGE || nSubType == TelephonyManager.NETWORK_TYPE_CDMA
+					&& !telephonyManager.isNetworkRoaming()) {
+				netType = "2G";
+			} else {
+				netType = "2G";
+			}
+		}
+		return netType;
 	}
 }
